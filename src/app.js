dbe4a46f8f00cd2b0ad03ebe4cf5477cad6c2670
Shoktapus <sinclair8075@gmail.com>
2022-08-04 21:42:09 +0000

    Building the random card project

--- src/app.js
+++ src/app.js
@@ -2,10 +2,20 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+let randomNum = numbers[Math.floor(Math.random() * numbers.length)];
+const suits = ["♦", "♥", "♠", "♣"];
+let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+if (randomSuit === suits[0] || randomSuit === suits[1]) {
+  document.querySelector("#suit").style.color = "red";
+  document.querySelector("#suitTwo").style.color = "red";
+}
+//get element id would also work in line 12 but the # isn't necessary
+// document.getElementById("demo").innerHTML = randomNum;
+document.querySelector("#demo").innerHTML = randomNum;
+document.querySelector("#suit").innerHTML = randomSuit;
+document.querySelector("#suitTwo").innerHTML = randomSuit;

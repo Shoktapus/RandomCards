0466bf81b2bbd389901b970a874c824c917824f0
Shoktapus <sinclair8075@gmail.com>
2022-08-06 00:19:54 +0000

    Fixed some issues with the way the suits were facing.

--- src/app.js
+++ src/app.js
@@ -4,18 +4,19 @@ import "./style.css";
 
 window.onload = function() {
   //write your code here
-};
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-let randomNum = numbers[Math.floor(Math.random() * numbers.length)];
-const suits = ["♦", "♥", "♠", "♣"];
-let randomSuit = suits[Math.floor(Math.random() * suits.length)];
-if (randomSuit === suits[0] || randomSuit === suits[1]) {
-  document.querySelector("#suit").style.color = "red";
-  document.querySelector("#suitTwo").style.color = "red";
-}
-//get element id would also work in line 12 but the # isn't necessary
-// document.getElementById("demo").innerHTML = randomNum;
-document.querySelector("#demo").innerHTML = randomNum;
-document.querySelector("#suit").innerHTML = randomSuit;
-document.querySelector("#suitTwo").innerHTML = randomSuit;
+  const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let randomNum = numbers[Math.floor(Math.random() * numbers.length)];
+  const suits = ["♦", "♥", "♠", "♣"];
+  let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  //Checking if suit is in the first or second index and then making it red.
+  if (randomSuit === suits[0] || randomSuit === suits[1]) {
+    document.querySelector("#suit").style.color = "red";
+    document.querySelector("#suitTwo").style.color = "red";
+  }
+  //get element id would also work in line 12 but the # isn't necessary
+  // document.getElementById("demo").innerHTML = randomNum;
+  document.querySelector("#demo").innerHTML = randomNum;
+  document.querySelector("#suit").innerHTML = randomSuit;
+  document.querySelector("#suitTwo").innerHTML = randomSuit;
+};
